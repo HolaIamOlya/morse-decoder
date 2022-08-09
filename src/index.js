8e182faa74f3fad76e4be770dd0cf4902f0b907f
olya <olyamolchanova322@gmail.com>
2022-08-09 19:13:49 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -38,8 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let result = ""
+    for(let i = 0, k = 10; i < expr.length; i = i + 10, k = k + 10){
+        let fromNumber = "";
+        fromNumber = expr.slice(i, k);
+        let mors = "";
+        if(fromNumber === "**********"){
+            mors = mors + " ";
+        }else{
+            for(let a = 0, b = 2; a < 10; a = a + 2, b = b + 2){
+                if(fromNumber.slice(a, b) === "10"){
+                    mors = mors + ".";
+                }
+                else if(fromNumber.slice(a, b) === "11"){
+                    mors = mors + "-";
+                }
+            }  
+        }
+        if(mors === " "){
+            result = result + " ";
+        }else{
+            result = result + MORSE_TABLE[mors]
+        }
+    }
+    return result
+    }
 
 module.exports = {
     decode
